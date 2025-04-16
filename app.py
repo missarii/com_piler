02b7706b96a3f7f85860deb5f0c697a8ab590bbf
missarii <143264692+missarii@users.noreply.github.com>
2025-04-16 16:13:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -202,5 +202,5 @@ def handle_disconnect():
             pass
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=3000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
